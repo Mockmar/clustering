bd282834ee13507452411f4fe59d3f31addf0818
root <root@LAPTOP-R401FK90>
2024-07-25 10:29:43 +0200

    avec visualisation des cluster

--- clustering.py
+++ clustering.py
@@ -37,3 +37,31 @@ print(df)
 # Évaluation des clusters
 score = silhouette_score(df_scaled, kmeans.labels_)
 print(f'Silhouette Score: {score}')
+
+
+# Réduction de dimensions avec t-SNE
+tsne = TSNE(n_components=2, random_state=0)
+df_tsne = tsne.fit_transform(df_scaled)
+
+# Réduction de dimensions avec PCA (alternative)
+pca = PCA(n_components=2)
+df_pca = pca.fit_transform(df_scaled)
+
+# Visualisation des clusters
+plt.figure(figsize=(12, 6))
+
+# t-SNE plot
+plt.subplot(1, 2, 1)
+plt.scatter(df_tsne[:, 0], df_tsne[:, 1], c=df['cluster'], cmap='viridis', marker='o')
+for i, txt in enumerate(df.index):
+    plt.annotate(txt, (df_tsne[i, 0], df_tsne[i, 1]))
+plt.title('t-SNE')
+
+# PCA plot
+plt.subplot(1, 2, 2)
+plt.scatter(df_pca[:, 0], df_pca[:, 1], c=df['cluster'], cmap='viridis', marker='o')
+for i, txt in enumerate(df.index):
+    plt.annotate(txt, (df_pca[i, 0], df_pca[i, 1]))
+plt.title('PCA')
+
+plt.show()
